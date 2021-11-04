05100f8f49c06fb416b875506f1e1c387297a75b
Christopher DuBois <chdubois@cisco.com>
2021-11-04 02:00:27 -0700

    fix(cms): fix env

--- common/cms/index.ts
+++ common/cms/index.ts
@@ -3,6 +3,12 @@ import {createClient, EntryCollection} from 'contentful';
 import {ContentTypeQuery, HeroImage, ICMS} from '../../types/cms';
 import {IHeroFields} from '../../types/cms/generated/contentful';
 
+console.log(
+  'resolve here',
+  process.env.VERCEL_ENV === 'preview'
+    ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
+    : process.env.CONTENTFUL_ACCESS_TOKEN
+);
 export class cms implements ICMS {
   private client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID as string,
