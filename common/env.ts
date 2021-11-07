9ccf02ce171b012168566f8059649c11470e5412
Christopher DuBois <chdubois@cisco.com>
2021-11-07 14:37:11 -0800

    feat(content): fix serializable content

--- common/env.ts
+++ common/env.ts
@@ -10,12 +10,12 @@ export type SerializableEnvironment = {
 
 export const getSerializableEnvironment = (env: any): SerializableEnvironment => {
   return {
-    cmsMgmtApiKey: env.CONTENTFUL_MANAGEMENT_API_ACCESS_TOKEN,
-    cmsApiKey: env.CONTENTFUL_ACCESS_TOKEN,
-    cmsSpaceId: env.CONTENTFUL_SPACE_ID,
-    cmsPreviewApiKey: env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
-    vercelEnv: env.VERCEL_ENV,
-    mapsApiKey: env.MAPS_API_KEY,
-    cmsEnv: env.CONTENTFUL_ENVIRONMENT,
+    cmsMgmtApiKey: env.CONTENTFUL_MANAGEMENT_API_ACCESS_TOKEN || '',
+    cmsApiKey: env.CONTENTFUL_ACCESS_TOKEN || '',
+    cmsSpaceId: env.CONTENTFUL_SPACE_ID || '',
+    cmsPreviewApiKey: env.CONTENTFUL_PREVIEW_ACCESS_TOKEN || '',
+    vercelEnv: env.VERCEL_ENV || '',
+    mapsApiKey: env.MAPS_API_KEY || '',
+    cmsEnv: env.CONTENTFUL_ENVIRONMENT || '',
   };
 };
