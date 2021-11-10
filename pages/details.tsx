e0b3ebcb7254b614097674e255886c9b9f729b73
Christopher DuBois <chdubois@cisco.com>
2021-11-09 21:48:33 -0800

    fix(details): ordering

--- pages/details.tsx
+++ pages/details.tsx
@@ -35,7 +35,17 @@ const Details = ({mapsApiKey, details}: {mapsApiKey: string; details: DetailsDat
   return (
     <Container component="section" maxWidth="md" sx={{mb: 5, mt: 5}}>
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={12} md={6} container flexDirection="column" justifyContent="center" alignItems="center">
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          md={6}
+          container
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          order={{xs: 1, sm: 1}}
+        >
           <Typography component="h2" variant="h4" textAlign="center" gutterBottom>
             {dayjs(details.eventDate).format('dddd MMMM DD YYYY h:mm A')}
           </Typography>
@@ -69,19 +79,18 @@ const Details = ({mapsApiKey, details}: {mapsApiKey: string; details: DetailsDat
               </Tooltip>
             </div>
           </ClickAwayListener>
-
           <Typography textAlign="center">
             {`Sara and Penny and Chris live in Black Diamond. We love you all !`}
           </Typography>
         </Grid>
-        <Grid item xs={12} sm={12} md={6}>
+        <Grid item xs={12} sm={6} md={6} order={{xs: 2, sm: 2}}>
           <Maps
             apiKey={mapsApiKey}
             pos={{lat: details.eventLocation?.lat || 0, lng: details.eventLocation?.lon || 0}}
             zoom={10}
           />
         </Grid>
-        <Grid item xs={12} sm={6} md={6}>
+        <Grid item xs={12} sm={6} md={6} order={{xs: 4, sm: 3}}>
           <Image
             src={`https:${details.venueImageMain?.fields.file.url}`}
             alt="venue"
@@ -90,13 +99,32 @@ const Details = ({mapsApiKey, details}: {mapsApiKey: string; details: DetailsDat
             height={600}
           />
         </Grid>
-        <Grid item xs={12} sm={6} md={6} container flexDirection="column" justifyContent="center" alignItems="center">
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          md={6}
+          container
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          order={{xs: 3, sm: 4}}
+        >
           <Typography component="h2" variant="h4" textAlign="center" gutterBottom>
             {details.venueTitle}
           </Typography>
           <Typography textAlign="center">{details.venueDescription}</Typography>
         </Grid>
-        <Grid item xs={12} sm={6} container flexDirection="column" justifyContent="center" alignItems="center">
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          container
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          order={{xs: 5, sm: 5}}
+        >
           <Typography component="h2" variant="h4" textAlign="center" gutterBottom>
             {details.rsvpTitle}
           </Typography>
@@ -109,13 +137,22 @@ const Details = ({mapsApiKey, details}: {mapsApiKey: string; details: DetailsDat
             </Button>
           </StyledLink>
         </Grid>
-        <Grid item xs={12} sm={6}>
+        <Grid item xs={12} sm={6} order={{xs: 6, sm: 6}}>
           <Image src="/static/wedding.svg" alt="about" layout="responsive" width={800} height={600} />
         </Grid>
-        <Grid item xs={12} sm={6}>
+        <Grid item xs={12} sm={6} order={{xs: 8, sm: 7}}>
           <Image src="/static/undraw_gifts_re_97j6.svg" alt="about" layout="responsive" width={800} height={600} />
         </Grid>
-        <Grid item xs={12} sm={6} container flexDirection="column" justifyContent="center" alignItems="center">
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          container
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          order={{xs: 7, sm: 8}}
+        >
           <Typography component="h2" variant="h4" textAlign="center" gutterBottom>
             {details.giftTitle}
           </Typography>
