3a5eadb4829c0db0f862255740029d9b39e90e7e
Christopher  Dubois <christopherdubois@ChristophersMBP.localdomain>
2021-12-27 21:32:16 -0800

    fix(rsvp): no supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -8,4 +8,8 @@ import {createClient} from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+let supabase = {};
+if (supabaseUrl && supabaseAnonKey) {
+  supabase = createClient(supabaseUrl, supabaseAnonKey);
+}
+export default supabase;
