f88f67a904eebd12927eb87c568a5978b451744f
chrisadubois <duboischris10@gmail.com>
2022-02-13 20:21:33 -0800

    feat(gallery): add lightbox (#25)
    Co-authored-by: Christopher  Dubois <christopherdubois@ChristophersMBP.localdomain>

--- components/Images/index.tsx
+++ components/Images/index.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import ImageList from '@mui/material/ImageList';
 import {useTheme} from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import {Container} from '@mui/material';
 import ImageItem from '../Image';
 import {GalleryData} from '../../types/cms';
+import {useState} from 'react';
+import {Modal, Box} from '@mui/material';
+import ImageCarousel from '../ImageCarousel';
 
 // TODO: infinite scroll
 const Images = ({images}: {images: GalleryData}) => {
@@ -14,6 +16,8 @@ const Images = ({images}: {images: GalleryData}) => {
   const matchesMdUp = useMediaQuery(theme.breakpoints.up('md'));
   const matchesMdDown = useMediaQuery(theme.breakpoints.down('md'));
   const matchesSmDown = useMediaQuery(theme.breakpoints.down('sm'));
+  const [openCarousel, setOpenCarousel] = useState(false);
+  const [carouselIndex, setCarouselIndex] = useState(-1);
 
   let columns = 3;
   if (matchesMdUp) {
@@ -24,28 +28,39 @@ const Images = ({images}: {images: GalleryData}) => {
     columns = 3;
   }
 
+  const imageItemHandler = (event: React.MouseEvent<HTMLElement>, index: number): void => {
+    setCarouselIndex(index);
+    setOpenCarousel(true);
+  };
+
   return (
-    <Container component="section" maxWidth="xl" sx={{mb: 5, mt: 5}}>
-      <Box
-        sx={{
-          width: '100',
-          height: '100%',
-        }}
-      >
-        <ImageList sx={{width: '100%', height: '100%'}} variant="masonry" cols={columns} gap={8}>
-          {images?.map((image, i) => (
-            <ImageItem
-              key={`${image.fields.title}-${i}`}
-              item={{
-                img: `https:${image.fields.file.url}`,
-                title: image.fields.title,
-                details: image.fields.file.details,
-              }}
-            />
-          )) || <span></span>}
-        </ImageList>
-      </Box>
-    </Container>
+    <>
+      <ImageCarousel isOpen={openCarousel} setOpen={setOpenCarousel} images={images} initialIndex={carouselIndex} />
+      <Container component="section" maxWidth="xl" sx={{mb: 5, mt: 5}}>
+        <Box
+          sx={{
+            width: '100',
+            height: '100%',
+          }}
+        >
+          <ImageList sx={{width: '100%', height: '100%'}} variant="masonry" cols={columns} gap={8}>
+            {images?.map((image, i) => (
+              <ImageItem
+                onClickHandler={(e) => {
+                  imageItemHandler(e, i);
+                }}
+                key={`${image.fields.title}-${i}`}
+                item={{
+                  img: `https:${image.fields.file.url}`,
+                  title: image.fields.title,
+                  details: image.fields.file.details,
+                }}
+              />
+            )) || <span></span>}
+          </ImageList>
+        </Box>
+      </Container>
+    </>
   );
 };
 
